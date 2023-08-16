36b278521db04ebfb0ff6773d72b4f98253fbe53
Hiro <gabriel.dc.cardozo@gmail.com>
2023-08-16 14:00:25 -0300

    Criação de código base do sistema bancario

--- potencia-tech-python/sistema-bancario-v1.py
+++ potencia-tech-python/sistema-bancario-v1.py
@@ -2,8 +2,48 @@
 #
 # Versão 1.0 do desafio do sistema bancário
 
+# Variaveis do programa
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+# Menu das opções
 menu = ("""
+-----------------------        
+Escolha uma das Opções
+[D] Despositar
+[E] Visualizar Extrato
+[S] Sacar
+[Q] Sair
+-----------------------        
+=>  """)
 
-print
+# Inicio do programa
+print("""
+Bem-vindo ao nosso novo sistema bancário""")
 
-""")
+while True:
+    # Estrutura condicional das funções do sistema
+    opcao = input(menu).lstrip().lower()
+
+    # Condição para a função "Depositar"
+    if opcao[0] == "d":
+        print(opcao)
+
+    # Condição para a função "Visulizar extrato"    
+    elif opcao[0] == "e":
+        print(opcao)
+    
+    # Condição para a função "Sacar"
+    elif opcao[0] == "s":
+        print(opcao)
+        
+    # Condição para sair do programa
+    elif opcao[0] == "q": 
+        break
+
+    # Tratamento de erro de digitação
+    else:
+        print("Opção inválida, digite novamente!")
