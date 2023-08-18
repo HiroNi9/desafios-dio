1ecf4f8be2630e020959e0e71679c9a797a3d59a
Hiro <gabriel.dc.cardozo@gmail.com>
2023-08-17 23:01:02 -0300

    Finalização - sistema bancario v1

--- potencia-tech-python/sistema-bancario-v1.py
+++ potencia-tech-python/sistema-bancario-v1.py
@@ -30,28 +30,52 @@ while True:
     # Condição para a função "Depositar"
     if opcao[0] == "d":
 
-        deposito = float(input("Quanto deseja depositar? =>R$ "))
+        valor_deposito = float(input("Quanto deseja depositar? =>R$ "))
 
-        if deposito > 0:
+        if valor_deposito > 0:
+            saldo += valor_deposito
+            extrato += f"Depósito realizado de R${valor_deposito:,.2f}\n"
 
-            saldo += deposito
-            extrato += f"Depósito realizado de R${deposito:,.2f}" + "\n"
+            print(f"\nVocê realizou um deposito de R${valor_deposito:,.2f}")
+            print(f"Saldo atual => R${saldo:,.2f}")
+
         else: 
             print("Valor incorreto, tente novamente...")
 
-    # Condição para a função "Visulizar extrato"    
-    elif opcao[0] == "e":
-        print(f"""
-------------------------------
-{extrato}------------------------------
-Saldo na conta: R${saldo:,.2f}""")
-    
     # Condição para a função "Sacar"
     elif opcao[0] == "s":
-        print(opcao)
+        valor_saque = float(input("Qual o valor que deseja sacar? => R$"))
 
+        if (numero_saques == LIMITE_SAQUES):
+            print("\nVocê utilizou o seu limite de saques diários, não é possível realizar mais saques.")
+        elif (valor_saque > limite):
+            print("\nO valor solicitado é maior que o valor limite disponível para saque, tente novamente.")
+        elif (valor_saque > saldo):
+            print("\nSaldo insuficiente, por favor, deposite mais fundos em sua conta ou saque um valor menor")
+
+        elif valor_saque > 0:
+            saldo -= valor_saque
+            extrato += f"Saque realizado de R${valor_saque:,.2f}\n"
+            numero_saques += 1
+
+            print(f"\nVocê realizou um saque de R${valor_saque:,.2f}")
+            print(f"Saldo atual => R${saldo:,.2f}")
+        
+        else:
+            print("Valor incorreto, tente novamente...")
+
+
+
+    # Condição para a função "Visulizar extrato"    
+    elif opcao[0] == "e":
+        print("=============Extrato=============")
+        print("Não foi realizadas nenhuma movimentação na conta" if not extrato else extrato)
+        print(f"Saldo na conta: R${saldo:,.2f}""")
+        print("=" * 33)
+        
     # Condição para sair do programa
     elif opcao[0] == "q": 
+        print("Obrigado por utilizar nosso sistema bancário!")
         break
 
     # Tratamento de erro de digitação
